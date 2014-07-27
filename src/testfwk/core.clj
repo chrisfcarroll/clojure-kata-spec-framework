c6ee9f3291676ef589655ea4d041d92e811bade7
chrisfcarroll <chris.carroll@unforgettable.me.uk>
2014-07-27 23:05:43 +0100

    Refactor after first Spec pass

--- src/testfwk/core.clj
+++ src/testfwk/core.clj
@@ -4,3 +4,14 @@
   "Execute a specification and reports failure"
   [name & more]
   (cons 'do more))
+
+(ns testfwk.core.bootstrap (:require [testfwk.core :refer :all]))
+ 
+  (def bootstrap-assertion 
+    '(let  [output  (with-out-str (spec "A dummy spec" (print "I was run")))]
+       (assert (= output "I was run"))
+       true))
+  (if (eval bootstrap-assertion)
+    (println "Bootstrapped OK.")
+    (throw (AssertionError. "Bootstrap assertion failed. Don't believe anything I claim has passed.")))
+ 
--- test/testfwk/core_test.clj
+++ test/testfwk/core_test.clj
@@ -1,26 +1,8 @@
 (ns testfwk.core-test
-  (:require 
-            [testfwk.core :refer :all]))
-
-  (ns testfwk.core-test.bootstrap (:require [testfwk.core :refer :all]))
- 
-    (def bootstrap-assertion 
-      '(let  [output  (with-out-str (spec "A dummy spec" (print "I was run")))]
-         (assert (= output "I was run"))
-         true))
-    (if (eval bootstrap-assertion)
-      (println "Bootstrapped OK.")
-      (throw (AssertionError. "Bootstrap assertion failed. spec failed to run its body.")))
-    ;--end bootstrap--
-
-(ns testfwk.core-test)
+  (:require [testfwk.core :refer :all] 
+             [testfwk.core.bootstrap :refer :all]))
 
 (spec "A Spec describes expected behaviour")
 
 (spec "A Spec runs its body" (eval bootstrap-assertion))
 
-
-(defn fake-fn-being-specced [] 
-  (let [s "I was run"]  
-    (print s) 
-    s))
